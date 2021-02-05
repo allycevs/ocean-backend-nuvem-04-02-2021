487c597a1650355ba39c5cff6a29bbe4db5eed97
allycevs <78514966+allycevs@users.noreply.github.com>
2021-02-04 20:09:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const bodyParser = require('body-parser');
 const { MongoClient, ObjectId } = require('mongodb');
 
 (async () => {
-    const url = 'mongodb://localhost:27017';
+    const url = 'mongodb+srv://admin:MVhyXlPJWaD2Cfbq@cluster0.7fu0x.mongodb.net/ocean_db?retryWrites=true&w=majority
+';
 
-    const dbName = 'ocean_database_03_02_2021';
+    const dbName = 'ocean_db';
 
     console.info('Conectando ao banco de dados...');
 
-    //const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
     console.info('MongoDB conectado com sucesso!');
 
-    //const db = client.db(dbName);
+    const db = client.db(dbName);
 
     const app = express()
 
